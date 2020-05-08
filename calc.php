049e3cc27505beed3ab4154c202a40c59abcaf24
Sasha <KuplennuySasha@i.ua>
2020-05-08 14:09:01 +0300

    first commit

---
+++ calc.php
@@ -0,0 +1,24 @@
+﻿<?php
+$x1 = $_GET['x1'];
+$x2 = $_GET['x2'];
+
+echo "При x1=$x1,<br>х2=$x2<br>";
+
+switch ($_GET['operation']) {
+    case '+':
+        $result = $x1 + $x2;
+        break;
+    case '-':
+        $result = $x1 - $x2;
+        break;
+    case '*':
+        $result = $x1 * $x2;
+        break;
+    case '/':
+        $result = $x1/$x2;
+        break;
+    default:
+        return 'Операція не підтримується';
+}
+echo "Результат становить $result";
+
